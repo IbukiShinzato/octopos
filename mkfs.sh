8dfbecb2b81d6166f4f6e65bb3de34e7245f60ab
Boran Seckin <boran@boranseckin.com>
2026-03-26 21:27:12 -0400

    fix: mkfs to use current dir

--- mkfs.sh
+++ mkfs.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e
 
 cargo build --release --package user
 
-pushd mkfs
-cargo run --release --target "$(rustc -vV | grep host | cut -d' ' -f2)" -- ../target/fs.img $(ls ../user/bin/*.rs | sed 's|../user/bin/\(.*\)\.rs|../target/riscv64gc-unknown-none-elf/release/\1|') ../LICENSE
-popd
+bins=$(find user/bin/*.rs | sed 's|user/bin/\(.*\)\.rs|target/riscv64gc-unknown-none-elf/release/\1|')
+
+cargo run \
+  --release \
+  --manifest-path mkfs/Cargo.toml \
+  --target "$(rustc -vV | grep host | cut -d' ' -f2)" -- \
+  target/fs.img \
+  $bins \
+  LICENSE \
+  "$@"
